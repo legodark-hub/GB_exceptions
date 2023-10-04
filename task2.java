7a606fc44ddc401b725a7cba59201948da76f64b
aidarminyazev <legodark@mail.ru>
2023-10-04 17:26:48 +0500

    task2 solution

---
+++ task2.java
@@ -0,0 +1,26 @@
+public class task2 {
+
+    // Если необходимо, исправьте данный код
+
+    // try {
+    // int d = 0;
+    // double catchedRes1 = intArray[8] / d;
+    // System.out.println("catchedRes1 = " + catchedRes1);
+    // } catch (ArithmeticException e) {
+    // System.out.println("Catching exception: " + e);
+    // }
+
+    public static void main(String[] args) {
+
+        int intArray[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e.getMessage());
+        }
+    }
+
+}
